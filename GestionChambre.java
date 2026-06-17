6e446291c3a86d3dae86ba9953263f4ad89a5afa
Diary HERINANDRASANA <hei.diary.4@gmail.com>
2026-06-17 20:57:53 +0300

    adding method

--- GestionChambre.java
+++ GestionChambre.java
@@ -3,14 +3,30 @@ package gestion_hotel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @AllArgsConstructor
 @Data
 public class GestionChambre {
-    float idChambre;
-    String categorieChambre;
-    disponibilte disponibilte;
+    private float idChambre;
+    private String categorieChambre;
+    private disponibilte disponibilite;
     private enum disponibilte {
-        libre,prie
+        libre,prie,reserve
     }
-
+    private LocalDate dateDebut;
+    private LocalDate dateFin;
+    public String getInfoPourGerant(){
+        if (this.disponibilite == disponibilte.libre){
+            return "La chambre est disponible";
+        }
+        return "Chambre occuoee du "+ dateDebut + " au " + dateFin;
+    }
+    public String getInfoPourClient(){
+        if (this.disponibilite == disponibilte.libre){
+            return "La chambre est disponible !";
+        } else {
+            return "Désolé, la chambre est déjà prise.";
+        }
+    }
 }

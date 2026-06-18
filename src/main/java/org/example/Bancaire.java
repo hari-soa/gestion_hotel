7ce98b59a47f32df3d54cd28f9d8b1b33d0ef65e
hari-soa <hei.zarasoa@gmail.com>
2026-06-18 18:17:49 +0300

    update : method

--- src/main/java/org/example/Bancaire.java
+++ src/main/java/org/example/Bancaire.java
@@ -4,16 +4,23 @@ import lombok.Getter;
 
 @Getter
 
-public class Bancaire extends CategorieChambre{
+public class Bancaire {
     private float montant;
+    private float solde;
+    private String recue;
 
-    public Bancaire(float prix) {
-        super(prix);
-    }
-
-    public void verser(float argent) {
-        if (argent > 0) {
-            this.montant += argent;
+    public void retirer(float montant) {
+        if (montant > 0 && montant <= solde) {
+            this.solde -= montant;
         }
     }
+
+    public void transferer(float montant, Gerant destination) {
+        if (montant > 0 && this.solde >= montant) {
+            this.retirer(montant);
+            destination.verser(montant);
+        }
+    }
+
+
 }
--- src/main/java/org/example/Gerant.java
+++ src/main/java/org/example/Gerant.java
@@ -6,11 +6,23 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 
-public class Gerant {
+public class Gerant extends Bancaire {
     private String nom;
     private String prenom;
     private String contact;
     private TypePayement typePayement;
+    private float compteBancaireHotel;
+    private float numeroMobileHotel;
+
+    public void verser(float montant) {
+        if (montant > 0) {
+            this.compteBancaireHotel += montant;
+        }
+    }
+
+
 }
+
+
 
 
